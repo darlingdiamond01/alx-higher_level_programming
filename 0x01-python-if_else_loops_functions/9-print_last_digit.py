b2f303724e4a5de5dff8004e87e20f71e23e0092
darlingdiamond01 <darlingmaria01@gmail.com>
2023-11-28 07:30:33 -0800

    last

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit)
+    if number > 0:
+        last = number % 10
+    else:
+        last = (number * -1) % 10
+    print("{:d}".format(last), end="")
+    return(last)
