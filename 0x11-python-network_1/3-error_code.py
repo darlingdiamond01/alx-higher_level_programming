c25228a6e7ba387acde55321b2fb8d3922836701
darlingdiamond01 <darlingmaria01@gmail.com>
2024-03-28 00:47:15 -0700

    patrick

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''sends a request to the URL and displays the body of the response.
+
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+'''
+import urllib.request
+import urllib.error
+import sys
